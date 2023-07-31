a20165b771921e4623282ef27446cbdc3140f922
Alam <alamkhalid0803@gmail.com>
2023-07-31 17:25:15 +0500

    Completed Benefits Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { SelectedPage } from "@/shared/types";
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -30,6 +31,7 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
